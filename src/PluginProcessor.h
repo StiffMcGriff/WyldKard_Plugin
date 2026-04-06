1b430f6685f08f9b1c3f67cad6e525418ac4ca8b
StiffMcGriff <38577944+StiffMcGriff@users.noreply.github.com>
2026-04-06 16:24:59 -0500

    Add WyldKardAudioProcessor class definition

---
+++ src/PluginProcessor.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+//==============================================================================
+class WyldKardAudioProcessor  : public juce::AudioProcessor
+{
+public:
+    //==============================================================================
+    WyldKardAudioProcessor();
+    ~WyldKardAudioProcessor() override;
+
+    //==============================================================================
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+
+    #ifndef JucePlugin_PreferredChannelConfigurations
+     bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+    #endif
+
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
+    //==============================================================================
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    //==============================================================================
+    const juce::String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
+
+    //==============================================================================
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const juce::String getProgramName (int index) override;
+    void changeProgramName (int index, const juce::String& newName) override;
+
+    //==============================================================================
+    void getStateInformation (juce::MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+
+    // --- Added for WyldKard Functionality ---
+    void previewSample (const juce::String& sampleName);
+
+private:
+    // Audio Handling Members
+    juce::AudioFormatManager formatManager;
+    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+    juce::AudioTransportSource transportSource;
+
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (WyldKardAudioProcessor)
+};
